32aa6b21738f083e5a49c1e2da5691a614618f5d
petr0vsk <petr0vskjy.aleksander@gmail.com>
2024-02-29 10:34:21 +0300

    добавим русские названия колонок в Задачу 2

--- pyspark_1.py
+++ pyspark_1.py
@@ -1,26 +1,28 @@
 # Нетология ДЗ
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, sum, desc, max, lag, desc, coalesce, lit
+from pyspark.sql.functions import col, sum, desc, max, lag, desc, coalesce, lit, format_number
 from pyspark.sql.window import Window
 from pyspark.sql.functions import rank, coalesce
 # Инициализация SparkSession
 spark = SparkSession.builder.appName("covid_analysis").getOrCreate()
 # Загрузка данных
 df = spark.read.csv('/home/petr0vsk/WorkSQL/Netology_Spark/Z_2/covid-data.csv', header=True, inferSchema=True)
-############################################################################
-# Задача 1
+## Задача 1 ##########################################################################
 # Для расчета процента переболевших населения страны без прямых данных о населении, 
 # мы можем использовать колонку total_cases_per_million, 
 # так как она отражает общее количество случаев на миллион населения. 
 # Используя эту колонку, мы можем оценить процент переболевших относительно общего числа населения, 
 # предполагая что 1 миллион в этой колонке соответствует 1 миллиону населения. 
 # Это позволит нам обойтись без явных данных о населении каждой страны.
+
 # Фильтрация данных на 31 марта
 df_filtered = df.filter(df['date'] == '2020-03-31')
 # Преобразование колонки total_cases_per_million в числовой формат
 df_filtered = df_filtered.withColumn("total_cases_per_million", df_filtered["total_cases_per_million"].cast("float"))
 # Расчет процента переболевших, используя total_cases_per_million
 df_filtered = df_filtered.withColumn("percentage_infected", col("total_cases_per_million") / 10000)
+# Округление процента переболевших до двух знаков после запятой
+df_filtered = df_filtered.withColumn("percentage_infected", format_number("percentage_infected", 2))
 # Сортировка стран по убыванию процента переболевших и выбор топ-15
 df_top15 = df_filtered.sort(col("percentage_infected").desc()).limit(15)
 # Применение алиасов к колонкам для итогового датасета
@@ -34,15 +36,25 @@ top_15_countries.show()
 output_path = '/home/petr0vsk/WorkSQL/Netology_Spark/Z_2/top_15_countries'
 # Сохранение DataFrame в CSV, если уже есть перезапишем
 top_15_countries.write.csv(output_path, header=True, mode="overwrite")
-##############################################################################
-# Задача 2
+### Задача 2 ###########################################################################
+#  Решаем исходя из предположения что задача заключается 
+# в поиске максимального единичного значения новых случаев за день в пределах последней недели марта.
+
 # Фильтрация данных за последнюю неделю марта 2021 года
 df_filtered = df.filter((col("date") >= "2021-03-24") & (col("date") <= "2021-03-31"))
 # Группировка по стране и дате, не агрегируем данные, чтобы сохранить детализацию по дням
 df_grouped = df_filtered.select("location", "date", "new_cases")
 # Находим день с максимальным количеством новых случаев для каждой страны
 windowSpec = Window.partitionBy("location").orderBy(desc("new_cases"))
-df_max_cases_per_country = df_grouped.withColumn("rank", rank().over(windowSpec)).filter(col("rank") == 1).drop("rank")
+df_max_cases_per_country = df_grouped.withColumn("rank", rank().over(windowSpec))\
+    .filter(col("rank") == 1)\
+    .drop("rank")
+# Применение алиасов к колонкам
+df_max_cases_per_country = df_max_cases_per_country.select(
+    col("date").alias("число"),
+    col("location").alias("страна"),
+    col("new_cases").alias("кол-во новых случаев")
+)
 # Сортировка по убыванию количества новых случаев и выборка топ-10
 top_10_days = df_max_cases_per_country.orderBy(desc("new_cases")).limit(10)
 # Вывод результатов
@@ -74,5 +86,6 @@ output_df.show()
 output_path = '/home/petr0vsk/WorkSQL/Netology_Spark/Z_2/last_week_March'
 output_df.write.csv(output_path, header=True, mode="overwrite")
 
-
+# Останавливаем сессию
+spark.stop()
 
